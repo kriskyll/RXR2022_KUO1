d48c2fe83c4b6f11017c9d8072ed082c67a36502
Rene Ruotsalainen <rlruotsalainen@gmail.com>
2022-11-11 09:08:03 +0200

    serial

---
+++ main.ino
@@ -0,0 +1,13 @@
+void setup() {
+	Serial.begin(9600);
+}
+
+void loop() {
+	if (Serial.available() > 0) {
+		String data = Serial.readStringUntil('\n');
+
+		if (data == 'f') {
+			
+		}
+	}
+}
--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 # Main application loop and gpio connections
+
+import serial_conn as ser
+
+if __name__ == "__main__":
+
+    while True:
+        x  = input()
+        if x == "f":
+            ser.forward()
+        elif x == "s":
+            ser.stop()
+        elif x == "l":
+            ser.left()
+        elif x == "r":
+            ser.right()    
