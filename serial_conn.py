0beacb841649a0ac166cf826113256f79b902b52
Rene Ruotsalainen <rlruotsalainen@gmail.com>
2022-11-11 09:48:12 +0200

    eka ajo etiäpäi

--- serial_conn.py
+++ serial_conn.py
@@ -1,22 +1,22 @@
 # Serial connection here
 
-#import serial
+import serial
 
-#ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-#ser.reset_input_buffer()
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+ser.reset_input_buffer()
 
 def forward():
-    #ser.write(b"f\n")
+    ser.write(b"f\n")
     print("forward")
 
 def stop():
-    #ser.write(b"s\n")
+    ser.write(b"s\n")
     print("stop")
 
 def left():
-    #ser.write(b"l\n")
+    ser.write(b"l\n")
     print("left")
 
 def right():
-    #ser.write(b"r\n")
+    ser.write(b"r\n")
     print("right")
