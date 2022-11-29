a0f666f7905d76d5fd5b77a1c5fc9fe8e7708b92
Rene Ruotsalainen <rlruotsalainen@gmail.com>
2022-11-29 14:16:25 +0200

    speech alulle

---
+++ speech.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# NOTE: this example requires PyAudio because it uses the Microphone class
+
+import speech_recognition as sr
+
+# obtain audio from the microphone
+r = sr.Recognizer()
+with sr.Microphone(device_index=4) as source:
+    print("Say something!")
+    audio = r.listen(source)
+
+# recognize speech using Sphinx
+try:
+    print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+except sr.UnknownValueError:
+    print("Sphinx could not understand audio")
+except sr.RequestError as e:
+    print("Sphinx error; {0}".format(e))
+
